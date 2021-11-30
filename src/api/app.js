cfb82ad5e8447bd4653dc20b678da51445bf9fd8
Sarah <sarahgsb@hotmail.com>
2021-11-30 15:01:37 -0300

    começando o projeto :rocket:

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
