5ae408a9c8c03664d26e60c8d6d36373a20cae30
Sarah <sarahgsb@hotmail.com>
2021-12-06 23:12:05 -0300

    testando refatora model

--- src/models/recipesModel.js
+++ src/models/recipesModel.js
@@ -22,22 +22,41 @@ const getAll = () =>
 // };
 
 const getById = async (id) => {
-  const recipe = await connect()
-  .then((db) => db.collection('recipes'));
+  const recipe = await connect().then((db) => db.collection('recipes'));
   const recipes = await recipe.findOne({ _id: ObjectId(id) });
   return recipes;
 };
 
-const update = (id, name, ingredients, preparation) => {
-  connect().then((db) =>
-    db
-      .collection('recipes')
-      .updateOne(
-        { _id: ObjectId(id) },
-        { $set: { name, ingredients, preparation } },
-      ));
+// const update = (id, name, ingredients, preparation) => {
+//   connect().then((db) =>
+//     db
+//       .collection('recipes')
+//       .updateOne(
+//         { _id: ObjectId(id) },
+//         { $set: { name, ingredients, preparation } },
+//       ));
 
-  return { _id: id, name, ingredients, preparation };
+//   return { _id: id, name, ingredients, preparation };
+// };
+
+const update = async ({ name, ingredients, preparation }, id) => {
+  if (!ObjectId.isValid(id)) return null;
+  const recipe = await connect().then((db) =>
+    db.collection('recipes'));
+
+  await recipe.updateOne(
+    { _id: ObjectId(id) },
+    {
+      $set: { name, ingredients, preparation },
+    },
+  );
+
+  return {
+    name,
+    ingredients,
+    preparation,
+    id,
+  };
 };
 
 const deleteById = async (id) => {
@@ -52,8 +71,7 @@ const deleteById = async (id) => {
 const uploadImage = async ({ id, imageURL }) => {
   if (!ObjectId.isValid(id)) return null;
 
-  const recipe = await connect().then((db) =>
-    db.collection('recipes'));
+  const recipe = await connect().then((db) => db.collection('recipes'));
 
   await recipe.updateOne(
     { _id: ObjectId(id) },
